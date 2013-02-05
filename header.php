a0d6513d4c895c760f55138cb91b82c3ffb5df04
KarlKell <karlkell@karlkell-WellspringWorldwide.(none)>
2013-02-04 22:46:50 -0600

    Adding what's been finished so far (pretty much nothing)

---
+++ header.php
@@ -0,0 +1,5 @@
+<meta name="description" content="It's all about the beer in here." />
+<meta name="keywords" content="hoppin, stout, beer, brew, crew, review, preview" />
+<meta name="author" content="Karl Kell" />
+<meta name="charset" content="UTF-8" />
+<?php echo "<img class='logo' src='".INSTALL_ROOT."/img/logo.png' width='80px' />" 
---
+++ index.php
@@ -0,0 +1,11 @@
+<head>
+<?php 
+  //brief config
+  define('INSTALL_ROOT', '/wp-content/themes/hoppinstout');
+  //bring in header
+  get_header();
+?>
+</head>
+
+<body>
+</body>
